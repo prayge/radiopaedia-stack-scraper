a31c237c68546ce0fd4739ae45b80915b4d0c5cb
prayge <sgjfxvs@gmail.com>
2022-07-08 09:48:03 +0100

    single image download

---
+++ scraper.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup as soup
+import requests
+from selenium import webdriver
+import chromedriver_autoinstaller
+from utils import download
+
+url = "https://radiopaedia.org/cases/normal-brain-mri-6"
+url2 = "https://radiopaedia.org/cases/retrosternal-multinodular-goitre-cervicothoracic-sign?lang=gb"
+filename = "test"
+
+chromedriver_autoinstaller.install()
+driver = webdriver.Chrome()
+driver.get(url)
+doc = soup(driver.page_source, "html.parser")
+divs = doc.find_all(
+    "div", class_="relative-fill offline-workflow-outer-wrapper")
+imgs = []
+
+for range in range(0, len(divs)):
+    if divs[0].img is not None:
+        imgs.append(divs[0].img['src'])
+
+download(imgs, filename)
---
+++ utils.py
@@ -0,0 +1,7 @@
+import requests
+
+def download (source, filename):
+    for enum, dl in enumerate(source):
+        img_data = requests.get(f"{dl}").content
+        with open("output/" + filename + f"-{enum}.jpg", 'wb') as handler:
+            handler.write(img_data)
