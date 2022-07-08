56f3a80515dc7a7a49d47ef2a19932d66615d909
prayge <sgjfxvs@gmail.com>
2022-07-08 10:14:21 +0100

    Single image

--- scraper.py
+++ scraper.py
@@ -4,9 +4,10 @@ from selenium import webdriver
 import chromedriver_autoinstaller
 from utils import download
 
+
 url = "https://radiopaedia.org/cases/normal-brain-mri-6"
 url2 = "https://radiopaedia.org/cases/retrosternal-multinodular-goitre-cervicothoracic-sign?lang=gb"
-filename = "test"
+filename = "test21"
 
 chromedriver_autoinstaller.install()
 driver = webdriver.Chrome()
@@ -16,8 +17,12 @@ divs = doc.find_all(
     "div", class_="relative-fill offline-workflow-outer-wrapper")
 imgs = []
 
+
 for range in range(0, len(divs)):
     if divs[0].img is not None:
+        print(divs[0].img)
         imgs.append(divs[0].img['src'])
+    else:
+        print("No img found")
 
 download(imgs, filename)
