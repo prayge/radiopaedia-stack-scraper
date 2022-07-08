bd3239fb060eb3c11ad21b8e2acd79b7bd9dc10e
prayge <sgjfxvs@gmail.com>
2022-07-08 13:22:27 +0100

    Scroll download working

--- scroll.py
+++ scroll.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup as soup
 import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import chromedriver_autoinstaller
 from utils import *
 
-case = "test"
+case = "brain"
+filename = "change-to-carousel"
 single_scroll = "https://radiopaedia.org/cases/normal-brain-mri-6"
 single_image = "https://radiopaedia.org/cases/metacarpophalangeal-joint-dislocations?lang=gb"
 multi_scroll = "https://radiopaedia.org/cases/retrosternal-multinodular-goitre-cervicothoracic-sign?lang=gb"
@@ -14,9 +18,20 @@ chromedriver_autoinstaller.install()
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=options)
-driver.get(single_image)
+driver.get(single_scroll)
 doc = soup(driver.page_source, "html.parser")
 
+try:
+    driver.execute_script("arguments[0].click();", WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="accept-choices"]'))))
+except:
+    print("Not found")
+
+try:
+    driver.execute_script("arguments[0].click();", WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/footer/div/div/div[1]/button'))))
+except:
+    print("Not found")
 
 for container in doc.find_all("div", "well case-section case-study"):
     # print(container)
@@ -24,6 +39,16 @@ for container in doc.find_all("div", "well case-section case-study"):
         print("Single page download...")
         image = container.find(
             "img", {"id": "offline-workflow-study-large-image"}).get("src")
-        download_single(image, case, 0)
+        download_single(image, filename, case,  0)
     else:
         print("Scroll downloader...")
+
+        for i in range(0, 5):
+            #driver.find_element(By.CLASS_NAME, "up").click()
+            driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, '//*[@id="case-images"]/div/div[3]/div[2]/div/div[2]/a[1]'))))
+            sel_image = driver.find_element("id",
+                                            "offline-workflow-study-large-image").get_attribute("src")
+            download_single(sel_image, filename, case, i)
+
+
--- utils.py
+++ utils.py
@@ -2,12 +2,13 @@ import requests
 import os
 
 
-def download_single(source, filename, num):
-    os.mkdir(filename)
+def download_single(source, filename, modality, num):
+    if not os.path.exists(modality):
+        os.mkdir(modality)
     img_data = requests.get(source).content
     if num > 10:
-        with open(f"{filename}" + "/" + filename + f"0{str(num)}.jpg", 'wb') as handler:
+        with open(f"{modality}" + "/" + filename + f"0{str(num)}.jpg", 'wb') as handler:
             handler.write(img_data)
     else:
-        with open(f"{filename}" + "/" + filename + f"{str(num)}.jpg", 'wb') as handler:
+        with open(f"{modality}" + "/" + filename + f"{str(num)}.jpg", 'wb') as handler:
             handler.write(img_data)
